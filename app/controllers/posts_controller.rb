0fe74c9ccdd4af6f5d953718b7273e68f05676db
kennytrionfo <kentrionfo@gmail.com>
2014-06-19 13:47:47 -0600

    todays notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,37 @@
 class PostsController < ApplicationController
+
   def index
-  end
+     @posts = Post.all
+   end
 
-  def show
-  end
 
-  def new
-  end
+   def show
+     @post = Post.find(params[:id])
+   end
+
+
+   def new
+     @post = Post.new
+   end
+
+
+   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+        redirect_to posts_path
+    else
+        render :new
+    end
+end
+
+
+private
+
+def post_params
+    params.require(:post).permit(:title, :author, :body)
+end
+
+   
+
 end
